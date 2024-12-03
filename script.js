84c7efd140ff8f07ba5d1e0f41581d2977ac20af
mpatsche <42234573+mpatsche@users.noreply.github.com>
2024-12-03 15:17:14 +0100

    added Louises cerberis

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#card--temperatur-slider").roundSlider({
+    radius: 100,
+    circleShape: "full",    // Cirkulær form
+    sliderType: "min-range",
+    mouseScrollAction: true,
+    value: 19,
+    handleSize: "+5",
+    min: 10,
+    max: 35,
+    startAngle: 270        // Starter i bunden
+});
